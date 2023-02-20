c0db8fe76974a9cb7ff91672c159e3603c61abd9
hugo <hugo.perier@cern.ch>
2023-02-20 22:49:09 +0100

    add more tests

--- test/gamepad.test.js
+++ test/gamepad.test.js
@@ -1,4 +1,4 @@
-import gamepad from '../src/gamepad';
+import gamepad, { DefaultGamepad } from '../src/gamepad';
 import { gamepadMock } from './mock.gamepad.test';
 
 describe('test gamepad handler', () => {
@@ -7,5 +7,38 @@ describe('test gamepad handler', () => {
     expect(gp).toBeDefined();
     expect(gp.id).toBe(gamepadMock.index);
     expect(gp.axeValues.length).toBe(gamepadMock.axes.length);
+  });
+
+  test('Check button changed invoke cb', () => {
+    const onCalled = jest.fn();
+    const gp = gamepad.init(gamepadMock);
+    gp.on(DefaultGamepad.KeyA, function () {
+      onCalled();
+    });
+    gp.buttonActions[1].changed.trigger();
+    expect(onCalled).toHaveBeenCalled();
+  });
+
+  test('Check button changed does not invoke cb', () => {
+    const onCalled = jest.fn();
+    const gp = gamepad.init(gamepadMock);
+    gp.on(DefaultGamepad.KeyA, function () {
+      onCalled();
+    });
+    gp.buttonActions[3].changed.trigger();
+    gp.buttonActions[7].changed.trigger();
+    gp.buttonActions[0].changed.trigger();
+    expect(onCalled).not.toHaveBeenCalled();
+  });
+
+  test('Unsubscribe event', () => {
+    const onCalled = jest.fn();
+    const gp = gamepad.init(gamepadMock);
+    gp.on(DefaultGamepad.KeyA, function () {
+      onCalled();
+    });
+    gp.off(DefaultGamepad.KeyA)
+    gp.buttonActions[1].changed.trigger();
+    expect(onCalled).not.toHaveBeenCalled();
   });
 });
