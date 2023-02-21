c0df54bab157f986c129d3a75559b4c5d22806a1
hugo <hugo.perier@cern.ch>
2023-02-21 22:56:27 +0100

    gamepad test

--- test/gamepad.test.js
+++ test/gamepad.test.js
@@ -1,8 +1,14 @@
 import gamepad, { DefaultGamepad } from '../src/gamepad';
-import { gamepadMock } from './mock.gamepad.test';
+import { getMock } from './mock.gamepad';
+
+const wait = async (t) =>
+  new Promise((res) => {
+    setTimeout(res, t);
+  });
 
 describe('test gamepad handler', () => {
   test('initialize', () => {
+    const gamepadMock = getMock();
     const gp = gamepad.init(gamepadMock);
     expect(gp).toBeDefined();
     expect(gp.id).toBe(gamepadMock.index);
@@ -11,6 +17,7 @@ describe('test gamepad handler', () => {
 
   test('Check button changed invoke cb', () => {
     const onCalled = jest.fn();
+    const gamepadMock = getMock();
     const gp = gamepad.init(gamepadMock);
     gp.on(DefaultGamepad.KeyA, function () {
       onCalled();
@@ -21,6 +28,7 @@ describe('test gamepad handler', () => {
 
   test('Check button changed does not invoke cb', () => {
     const onCalled = jest.fn();
+    const gamepadMock = getMock();
     const gp = gamepad.init(gamepadMock);
     gp.on(DefaultGamepad.KeyA, function () {
       onCalled();
@@ -31,14 +39,68 @@ describe('test gamepad handler', () => {
     expect(onCalled).not.toHaveBeenCalled();
   });
 
-  test('Unsubscribe event', () => {
-    const onCalled = jest.fn();
+  test('cycle check status', () => {
+    const gamepadMock = getMock();
     const gp = gamepad.init(gamepadMock);
-    gp.on(DefaultGamepad.KeyA, function () {
-      onCalled();
-    });
-    gp.off(DefaultGamepad.KeyA)
-    gp.buttonActions[1].changed.trigger();
-    expect(onCalled).not.toHaveBeenCalled();
+    const mockGamepads = () => [gamepadMock];
+    global.navigator = {
+      getGamepads: mockGamepads,
+    };
+    gp.checkStatus();
+    gamepadMock.buttons[0] = true;
+    gamepadMock.axes[0] = 0.18;
+    gp.checkStatus();
+    expect(gp.axeValues[0]).toBe(0.18);
+    expect(gp.axeValues[1]).toBe(0);
+    expect(gp.axeValues[2]).toBe(0);
+    expect(gp.axeValues[3]).toBe(0);
+  });
+
+  test('cycle check status events', () => {
+    const gamepadMock = getMock();
+    const gp = gamepad.init(gamepadMock);
+
+    const onButtonPressed = jest.fn();
+    const onJoystickEvent = jest.fn();
+
+    gp.on(DefaultGamepad.LeftJoystickAxeX, onJoystickEvent, 'pressed');
+    gp.on(DefaultGamepad.KeyA, onButtonPressed, 'changed');
+
+    global.navigator = {
+      getGamepads: () => {
+        const mock2 = getMock();
+        mock2.buttons[1] = {
+          pressed: true,
+        };
+        mock2.axes[0] = 0.18;
+        return [mock2];
+      },
+    };
+    gp.checkStatus();
+    expect(onJoystickEvent).toHaveBeenCalledTimes(1);
+    expect(onButtonPressed).toHaveBeenCalledTimes(1);
+    expect(onJoystickEvent).toHaveBeenCalledWith(0.18);
+  });
+
+  test('cycle check repeat event', async () => {
+    const gamepadMock = getMock();
+    const gp = gamepad.init(gamepadMock);
+
+    const onJoystickEvent = jest.fn();
+    gp.on(DefaultGamepad.LeftJoystickAxeX, onJoystickEvent, 'repeat');
+    global.navigator = {
+      getGamepads: () => {
+        const mock2 = getMock();
+        mock2.axes[0] = 0.18;
+        return [mock2];
+      },
+    };
+    const timer = setInterval(() => {
+      gp.checkStatus();
+    }, 1000 / 60); // 60fps to simulate the animate
+    const j = await wait(1000);
+    expect(onJoystickEvent.mock.calls.length).toBeGreaterThan(8);
+    expect(onJoystickEvent.mock.calls.length).toBeLessThan(11);
+    clearTimeout(timer);
   });
 });
